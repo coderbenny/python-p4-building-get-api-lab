7c768d2b9cf917cdb6ee1a1dc9287d7c6237074d
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-02-29 19:58:55 +0300

    done

--- server/app.py
+++ server/app.py
@@ -20,19 +20,67 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+    
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+    
+    response.headers["Content-Type"] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery_data = Bakery.query.filter_by(id=id).first()
+    
+    bakery = bakery_data.to_dict()
+    
+    if bakery_data:
+        response = make_response(
+            jsonify(bakery),
+            200
+        )
+        response.headers["Content-Type"] = 'application/json'
+        return response
+    else:
+        return jsonify({"error": "Bakery not found"}, 404)
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    all_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    
+    goods = [good.to_dict() for good in all_goods ]
+    
+    if goods:
+        response = make_response(
+            jsonify(goods),
+            200
+        )
+        response.headers["Content-Type"] = 'application/json'
+        return response
+    else:
+        return jsonify({"error": "No goods found"})
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    
+    if baked_goods:
+        expensive_good = baked_goods[0]
+        exp_good_dict = expensive_good.to_dict()
+    
+        response = make_response(
+            jsonify(exp_good_dict),
+            200
+        )
+        
+        response.headers["Content-Type"]='application/json'
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
